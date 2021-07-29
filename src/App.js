106f1e583a4e42ea0753e4384bfa6b57327cc72a
Rushi-45 <61156305+Rushi-45@users.noreply.github.com>
2021-07-29 18:33:35 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/base_components/Home'
 import About from './components/base_components/AboutMe'
 import Navbar1 from './components/base_components/Navbar1'
-import Navbar from './components/base_components/Navbar'
+
 
 
 function App() {
 
   return (
     <Router>     
-      <Navbar1 />
-      {/* <Navbar /> */}
+      <Navbar1 />      
 
       <Switch>
         <Route path="/" exact component={Home} />
--- src/components/base_components/Dropdown.js
+++ src/components/base_components/Dropdown.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 // import { MenuItems } from './MenuItems';
+// import { Link } from 'react-router-dom';
 import './Dropdown.css';
 
 function Dropdown() {
